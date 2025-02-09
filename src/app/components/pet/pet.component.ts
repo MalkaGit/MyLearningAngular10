4ba5a20a3d8663c8fd078dea751e1b47a6d498bb
regine <regine.issan@icloud.com>
2025-02-09 16:34:21 +0200

    Pet service layer Verion4 - async - _using promise try await catch

--- src/app/components/pet/pet.component.ts
+++ src/app/components/pet/pet.component.ts
@@ -9,10 +9,10 @@ import { PetService } from '../../services/pet.service'
   styleUrls: ['./pet.component.css']
 })
 export class PetComponent {
-  pets: Pet[] = [];
-  newOrUpdatedPet: Pet = { id: 0, name: '', breed: '', age: 0, weight: 0, photoUrl: '' };
-  editIndex: number | null = null;  
-
+  public errorMessage: string = '';  //the error from the service 
+  public pets: Pet[] = [];
+  public newOrUpdatedPet: Pet = { id: 0, name: '', breed: '', age: 0, weight: 0, photoUrl: '' };
+  public editIndex: number | null = null;  
 
    constructor(private petService: PetService) {}
 
@@ -20,25 +20,60 @@ export class PetComponent {
     this.getPets();
   }
 
-  getPets(): void {
-    this.pets = this.petService.getAll();
+  async getPets(): Promise<void> {
+    try {
+      this.pets = await this.petService.getAll();
+    } 
+    catch (error :any) {
+      this.errorMessage = error.message || 'Something went wrong!';
+    }
   }
+  // getPets(): void {
+  //   this.pets = this.petService.getAll();
+  // }
+
 
   // Add or Update a pet
-  addPet() {
-    if (this.editIndex === null) {
-      //add
-      const addedPet = this.petService.add(this.newOrUpdatedPet);
-      this.pets.push({ ...addedPet }); //create copy
-    } else {
-      //update
-      const updatedPet :Boolean = this.petService.update(this.newOrUpdatedPet);
-      if (updatedPet)
-        this.pets[this.editIndex] = { ...this.newOrUpdatedPet };//copy !
-      this.editIndex = null;
+  async addPet(): Promise<void> {
+    if (this.editIndex === null) 
+    { //add
+      try {
+          const addedPet = await this.petService.add(this.newOrUpdatedPet);
+          this.pets.push({ ...addedPet }); //create copy
+      } 
+      catch (error: any){
+          this.errorMessage = error.message || 'Failed to add item!';
+      }
+    } 
+    else 
+    { //update
+      try {
+          await this.petService.update(this.newOrUpdatedPet);
+          this.pets[this.editIndex] = { ...this.newOrUpdatedPet };//copy !
+          this.editIndex = null;
+      } 
+      catch (error: any){
+        this.errorMessage = error.message || 'Failed to update item!';
+      }
     }
     this.resetForm();
   }
+  // addPet() {
+  //   if (this.editIndex === null) {
+  //     //add
+  //     const addedPet = this.petService.add(this.newOrUpdatedPet);
+  //     this.pets.push({ ...addedPet }); //create copy
+  //   } else {
+  //     //update
+  //     const updatedPet :Boolean = this.petService.update(this.newOrUpdatedPet);
+  //     if (updatedPet)
+  //       this.pets[this.editIndex] = { ...this.newOrUpdatedPet };//copy !
+  //     this.editIndex = null;
+  //   }
+  //   this.resetForm();
+  // }
+
+  
 
   // Edit pet (loads data into the form)
   editPet(index: number) {
@@ -47,20 +82,44 @@ export class PetComponent {
   }
 
   // Delete pet
-  deletePet(index: number) {
-    const deletedPet :Boolean = this.petService.delete(this.pets[index].id);
-    if (deletedPet) {
+  async deletePet(index: number = 0): Promise<void> {
+    try 
+    {  
+      await this.petService.delete(this.pets[index].id);
       this.pets.splice(index, 1);
       if (this.editIndex === index) { //in case deleted pet is editted
-        this.resetForm();
+               this.resetForm();
       }
+    } catch (error :any ) 
+    {
+      this.errorMessage = error.message || 'Failed to delete item!';
     }
   }
+  // deletePet(index: number) {
+  //   const deletedPet :Boolean = this.petService.delete(this.pets[index].id);
+  //   if (deletedPet) {
+  //     this.pets.splice(index, 1);
+  //     if (this.editIndex === index) { //in case deleted pet is editted
+  //       this.resetForm();
+  //     }
+  //   }
+  // }
 
   // Reset the form
   resetForm() {
     this.newOrUpdatedPet = { id: 0, name: '', breed: '', age: 0, weight: 0, photoUrl: '' };
     this.editIndex = null;
+  }
+
+
+  //not used
+  async getItemById(id: number): Promise<void> {
+    try {
+      const pet = await this.petService.getById(id);
+      console.log(pet); // Do something with the item
+    } catch (error :any) {
+      this.errorMessage = error.message || 'Item not found!';
+    }
   }
 }
 
@@ -69,7 +128,6 @@ export class PetComponent {
   //constructor define a private service property
   //di: When Angular creates the component, the Dependency Injection system sets the service parameter to the singleton instance.
   //constructor(private petService: PetService) {}
-
 
   
  //Note: calling getPet in the constructor, is not the best practice.
--- src/app/services/pet.service.ts
+++ src/app/services/pet.service.ts
@@ -7,51 +7,105 @@ import { ParseTemplateOptions } from '@angular/compiler';
 })
 export class PetService {
   private pets: Pet[] = [
-    { id: 1, name: 'Buddy!!!', breed: 'Golden Retriever', age: 5, weight: 30, photoUrl: '' },
-    { id: 2, name: 'Luna!!!',  breed: 'Labrador', age: 3, weight: 25, photoUrl: '' }
+    { id: 1, name: 'Buddy:-)', breed: 'Golden Retriever', age: 5, weight: 30, photoUrl: '' },
+    { id: 2, name: 'Luna:-)',  breed: 'Labrador', age: 3, weight: 25, photoUrl: '' }
   ];
   private nextNewIndex: number = 3;
 
   constructor() {}
 
-  getAll(): Pet[] {
-    return [...this.pets];//retrun a copy to prevent direct modification
+  async getAll(): Promise<Pet[]> {
+    throw new Error("get all failed");
+    return [...this.pets]; // automatically resolves the promise. retrun a copy to prevent direct modification
   }
+  //sync verion
+  // getAll(): Pet[] {
+  //   return [...this.pets];
+  // }
 
-  getById(id: number): Pet |undefined {
+
+  async getById(id: number): Promise<Pet> {
     const pet :Pet|undefined = this.pets.find(p => p.id === id);
-    const result : Pet |undefined  = (pet === undefined) ?undefined :  {...pet };//return a copy to prevent direct modification
-    return result;
+    if (!pet) {
+      throw new Error(`Pet with id ${id} not found`);
+    }
+    return {...pet }; // automatically resolves the promise. return a copy to prevent direct modification.
   }
+  //sync verion
+  // getById(id: number): Pet |undefined {
+  //   const pet :Pet|undefined = this.pets.find(p => p.id === id);
+  //   const result : Pet |undefined  = (pet === undefined) ?undefined :  {...pet };
+  //   return result;
+  // }
 
-  add(pet: Pet): Pet {
+  async add(pet: Pet): Promise<Pet> {
+    if (!pet.name) {
+      throw new Error('Bad request: name is required'); //resolve to return Promise.reject ...
+    }
     const newPet: Pet = { ...pet}; //save a copy to prevent direct modification
     newPet.id = this.nextNewIndex++;
     this.pets.push(newPet);
-    return { ...newPet }; //retrun a copy to prevent direct modification
+    return { ...newPet };  //resolve to return Promise.resolve... retrun a copy to prevent direct modification
   }
+  //sync verion
+  //add(pet: Pet): Pet {
+  //   const newPet: Pet = { ...pet}; //save a copy to prevent direct modification
+  //   newPet.id = this.nextNewIndex++;
+  //   this.pets.push(newPet);
+  //   return { ...newPet }; //retrun a copy to prevent direct modification
+  // }
+  
 
-  update(pet: Pet): Boolean {
+
+  async update(pet: Pet): Promise<void> {
+    if (!pet.name) {
+      throw new Error('Bad request: Pet name is required');
+    }
     const index = this.pets.findIndex(p => p.id === pet.id);
     if (index !== -1) {
       this.pets[index] = {...pet };//save a copy to prevent direct modification
-      return true;
-    }
+      return; // Automatically resolves the promise as nothing needs to be returned
+    } 
     else {
-      return false;
-    }    
+      throw new Error(`Pet with id ${pet.id} not found`);
+    }
   }
+  //sync verion
+  // update(pet: Pet): Boolean {
+  //   const index = this.pets.findIndex(p => p.id === pet.id);
+  //   if (index !== -1) {
+  //     this.pets[index] = {...pet };//save a copy to prevent direct modification
+  //     return true;
+  //   }
+  //   else {
+  //     return false;
+  //   }    
+  // }
 
-  delete(id: number): boolean {
+
+
+  async delete(id: number): Promise<void> {
     const index = this.pets.findIndex(p => p.id === id);
     if (index !== -1) {
       this.pets.splice(index, 1);
-      return true; 
+      return; // Automatically resolves the promise as nothing needs to be returned
     }
-    else{
-     return false;
+    else 
+    {
+      throw new Error(`Pet with id ${id} not found`);
     }
   }
+  //sync verion
+  // delete(id: number): boolean {
+  //   const index = this.pets.findIndex(p => p.id === id);
+  //   if (index !== -1) {
+  //     this.pets.splice(index, 1);
+  //     return true; 
+  //   }
+  //   else{
+  //    return false;
+  //   }
+  // }
 
 }
 
@@ -60,8 +114,11 @@ export class PetService {
 
 //key features
 
+//new to promise 
+//1.  update and delete retrun void if item found. Otherwise, eception
 
-//0. ensure immutability by retruning copies when needed
+//as in array
+//0.    ensure immutability by retruning copies when needed
 //    ithout it, as array updated here it is updated in the component
 
 //1. @Injectable,import { Injectable }
@@ -69,11 +126,6 @@ export class PetService {
 //   The @Injectable() decorator accepts a metadata object for the service, the same way the @Component() decorator did for your component classes.
 //import { Injectable } from '@angular/core';
 
-
 //. add: Assigns a unique, auto-incrementing id to each item.
-
 //3.getItemById(id: number): Retrieves a specific item by its id.
 
-//4.updateItem(id: number, newName: string): Updates an item’s name by id.
-
-//5. deleteItem(id: number): Removes an item by id.
